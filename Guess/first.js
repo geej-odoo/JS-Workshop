3368b077c2f26f15b1a1e6a68b5ee6a1e04f757d
Sanchit Gupta <sagu@odooo.com>
2023-06-05 13:27:16 +0530

    Guess Number Game

---
+++ Guess/first.js
@@ -0,0 +1,24 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessCount = 0;
+
+function checkGuess() {
+  let guess = parseInt(document.getElementById("guessInput").value);
+  console.log(guess)
+  if(isNaN(guess) || guess<1 || guess>100)
+  {
+    alert("Please Enter Appropriate Value")
+    return false;
+  }
+
+  guessCount++;
+  let resultElement = document.getElementById("result");
+ 
+  if (guess > randomNumber) {
+    resultElement.textContent = "HIGH";
+  } else if (guess < randomNumber) {
+    resultElement.textContent = "LOW";
+  } else{
+    resultElement.textContent = "CORRECT GUESS - " + guessCount + " Trials";
+    document.getElementById("guessInput").disabled = true;
+  }
+}
